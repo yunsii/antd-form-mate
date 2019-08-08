0d908b09e8c15e55847110c323a8e62b37850b1c
xieyunsheng <xieyunsheng>
2019-08-08 11:23:34 +0800

    feat: 0.1.10 export components

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,10 @@ export type ComponentType = FormMate.ComponentType;
 export type CustomFormItemProps = FormMate.CustomFormItemProps;
 export type ItemConfig = FormMate.ItemConfig;
 
+export { default as DatePicker, CustomRangePicker as RangePicker } from './components/CustomDatePicker';
+export { default as Switch } from './components/CustomSwitch';
 export { default as AMap } from './components/CutomAMap';
+export { default as LocationPicker } from './components/LocationPicker';
+export { default as PicturesWall } from './components/PicturesWall';
+export { default as Select } from './components/Select';
+export { default as Upload, CustomDragger as Dragger } from './components/Upload';
