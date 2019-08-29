df3ac1eaefc771a0c1331378d0442964760f0f9e
Yuns Xie <yuns.xie@qq.com>
2019-08-29 22:25:45 +0800

    feat: cols

--- src/components/CustomRadioGroup/index.tsx
+++ src/components/CustomRadioGroup/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Radio } from 'antd';
+import { Radio, Row, Col } from 'antd';
 import { RadioGroupProps } from 'antd/lib/radio';
 import { CheckboxOptionType } from 'antd/lib/checkbox';
 
@@ -10,19 +10,37 @@ export interface Option extends CheckboxOptionType {
 }
 
 export interface CustomRadioGroupProps extends RadioGroupProps {
+  cols?: 1 | 2 | 3 | 4 | 6 | 8 | 12 | 24;
 }
 
 export default class CustomRadioGroup extends React.Component<CustomRadioGroupProps> {
   render() {
-    const { options = [], ...rest } = this.props;
+    const { cols, options = [], ...rest } = this.props;
+    if (!cols) {
+      return (
+        <Radio.Group {...rest}>
+          {options && (options as Option[]).map((item) => {
+            const { value, ...rest } = item;
+            return (
+              <Radio key={item.value} value={item.value} {...rest}>{item.text}</Radio>
+            )
+          })}
+        </Radio.Group>
+      )
+    }
+    const span = 24 / cols;
     return (
       <Radio.Group {...rest}>
-        {options && (options as Option[]).map((item) => {
-          const { value, ...rest } = item;
-          return (
-            <Radio key={item.value} value={item.value} {...rest}>{item.text}</Radio>
-          )
-        })}
+        <Row type="flex">
+          {options && (options as Option[]).map((item) => {
+            const { value, ...rest } = item;
+            return (
+              <Col key={`${item.value}`} span={span}>
+                <Radio value={item.value} {...rest}>{item.text}</Radio>
+              </Col>
+            )
+          })}
+        </Row>
       </Radio.Group>
     )
   }
