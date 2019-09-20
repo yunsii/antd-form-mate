ea521bceb89bb004de9d458ebc27603b09b00f8a
Yuns Xie <yuns.xie@qq.com>
2019-09-20 16:49:23 +0800

    feat: export components

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,14 @@ export const locale = {
   setPicturesWallLocale: FormMateLocale.setPicturesWallLocale,
   setDraggerLocale: FormMateLocale.setDraggerLocale,
 };
+
+export { default as CheckGroup} from './lib/components/CustomCheckGroup';
+export { default as DatePicker} from './lib/components/CustomDatePicker';
+export { default as RadioGroup} from './lib/components/CustomRadioGroup';
+export { default as Select} from './lib/components/CustomSelect';
+export { default as Switch} from './lib/components/CustomSwitch';
+export { default as Upload} from './lib/components/CustomUpload';
+export { default as Dragger} from './lib/components/CustomUpload/CustomDragger';
+export { default as AMap} from './lib/components/CustomAMap';
+export { default as LocationPicker} from './lib/components/LocationPicker';
+export { default as PicturesWall} from './lib/components/PicturesWall';
