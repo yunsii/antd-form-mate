4779e1e659706817a992863494f9c90adc0b7348
Yuns Xie <yuns.xie@qq.com>
2019-12-05 23:19:12 +0800

    chore: remove useless

--- stories/FormMate.ts
+++ stories/FormMate.ts
@@ -1,19 +1,1 @@
-import { config } from '../src';
-import { ComponentType } from '../src/lib/props';
-
-const { setDefaultExtra, setCommenProps } = config;
-
-setDefaultExtra({
-  picture: '自定义图片默认提示',
-});
-
-setCommenProps((type: ComponentType) => {
-  if (!(['check-group', 'textarea', 'switch'] as ComponentType[]).includes(type)) {
-    return {
-      allowClear: true,
-    }
-  }
-  return null;
-});
-
 export { createFormItems } from '../src';
