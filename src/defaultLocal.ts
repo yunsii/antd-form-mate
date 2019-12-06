bf70d2434c60d4719dac342e17bbf5a2ac1f5d58
Yuns Xie <yuns.xie@qq.com>
2019-12-06 22:12:41 +0800

    feat: defaultLocal

---
+++ src/defaultLocal.ts
@@ -0,0 +1,13 @@
+export default {
+  picturesWall: {
+    upload: '上传',
+  },
+  dragger: {
+    upload: '点击或拖拽文件到此处上传',
+  },
+  map: {
+    currentAddress: '当前地址：',
+    addressPickPlaceholder: '请选择地址',
+    addressInputPlaceholder: '请输入地址',
+  },
+}
