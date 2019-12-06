8f7e8b2b6e36b6986c708909d7212dcd4db1caf0
Yuns Xie <yuns.xie@qq.com>
2019-12-06 22:12:27 +0800

    feat: SetLocal

--- src/ConfigContext.ts
+++ src/ConfigContext.ts
@@ -2,15 +2,30 @@ import React from 'react';
 import _isFunction from 'lodash/isFunction';
 import { ComponentType } from './lib/props';
 
+export interface SetLocal {
+  picturesWall?: {
+    upload?: string;
+  };
+  dragger?: {
+    upload?: string;
+  };
+  map?: {
+    currentAddress?: string;
+    addressPickPlaceholder?: string;
+    addressInputPlaceholder?: string;
+  };
+}
+
 export interface ConfigContextProps {
   setCommenProps?: (type: ComponentType) => any;
-  commenExtra?: { [k in ComponentType]?: any };
-  commenRules?: { [k in ComponentType]?: any[] };
   uploadFn?: (file: File, setProgress: (percent: number) => any) => Promise<any>;
   getUrl?: (response: any) => { url: string, thumbUrl?: string };
   isUploadOk?: (response: any) => boolean;
+  commenExtra?: { [k in ComponentType]?: any };
+  commenRules?: { [k in ComponentType]?: any[] };
   pictureFormatLimit?: string;
   amapKey?: string;
+  setLocal?: SetLocal;
 }
 
 export const ConfigContext = React.createContext<ConfigContextProps>({})
