672e33772ddd50ab1718129dbc38e0222a398b9b
Yuns Xie <yuns.xie@qq.com>
2019-12-09 10:51:21 +0800

    fix: unify switch value

--- src/lib/components/CustomSwitch/index.tsx
+++ src/lib/components/CustomSwitch/index.tsx
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import { Switch } from "antd";
+import { setSwitchValue } from "../../setValue";
 
 export interface CustomSwitchProps {
   checked?: boolean | number;
 }
 
 export default class CustomSwitch extends Component<CustomSwitchProps> {
-  setValue = checked => !!checked;
 
   render() {
     const { checked, ...rest } = this.props;
-    return <Switch {...rest} checked={this.setValue(checked)} />;
+    return <Switch {...rest} checked={setSwitchValue(checked)} />;
   }
 }
