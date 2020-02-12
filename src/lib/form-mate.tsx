32ae8ab00d3c484ad8651f1cc97bbed1e9f353b6
Yuns Xie <yuns.xie@qq.com>
2020-02-13 00:31:03 +0800

    refactor: no form prop and no hidden type

--- src/lib/form-mate.tsx
+++ src/lib/form-mate.tsx
@@ -1,33 +1,21 @@
 import React from "react";
 import _get from 'lodash/get';
 import _isFunction from 'lodash/isFunction';
-import { WrappedFormUtils } from "antd/lib/form/Form";
 import RenderItem from "./item-components/RenderItem";
 import { ItemConfig, Layout } from "./props";
 
-export default (form: WrappedFormUtils) => (
+export default (
   itemsConfig: ItemConfig[],
   formLayout?: Layout,
 ) => {
-  if (!_isFunction(_get(form, 'getFieldDecorator'))) { throw new TypeError('GetFieldDecorator is not function.'); }
-  const { getFieldDecorator } = form;
-
   return itemsConfig.map(config => {
     const {
-      type = "string",
       field,
-      fieldProps,
     } = config;
-
-    if (type === 'hidden') {
-      getFieldDecorator(field, { initialValue: _get(fieldProps, 'initialValue') });
-      return null;
-    }
 
     return (
       <RenderItem
         key={field}
-        form={form}
         formLayout={formLayout}
         {...config}
       />
