ff0a8c57fa4ed11be2225a20a199c1aa935ba4b0
Yuns Xie <yuns.xie@qq.com>
2020-02-17 19:55:20 +0800

    feat: new intl solution

---
+++ src/locale/en_US.tsx
@@ -0,0 +1,13 @@
+export default {
+  picturesWall: {
+    upload: 'Upload',
+  },
+  dragger: {
+    upload: 'Click or drag file to this area to upload',
+  },
+  map: {
+    currentAddress: 'Current Address: ',
+    addressPickPlaceholder: 'Pick somewhere',
+    addressInputPlaceholder: 'Input ...',
+  },
+}
