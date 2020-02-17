4fe72641ddcdaabb8b30f5a7026757144e58e8d9
Yuns Xie <yuns.xie@qq.com>
2020-02-17 19:56:09 +0800

    refactor: use new intl

--- src/lib/item-components/PicturesWall/index.tsx
+++ src/lib/item-components/PicturesWall/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, forwardRef } from "react";
+import React, { useContext, useState } from "react";
 import { PlusOutlined } from "@ant-design/icons";
 import _pick from "lodash/pick";
 import _isArray from "lodash/isArray";
@@ -12,102 +12,76 @@ import CustomUpload, {
   filterFileList,
 } from "../../components/CustomUpload/index";
 // import { getBase64 } from '../../../utils';
-import ConfigContext from '../../../config-provider/context';
+import ConfigContext from '../../../config-context/context';
 import { setFileList } from '../../setValue';
 import styles from "./index.less";
+import { useIntl } from "../../../intl-context";
 
 export interface PicturesWallProps extends CustomUploadPorps {
   value?: string | any[];
   viewerProps?: ViewerProps;
-}
-
-export interface InternalPicturesWallProps extends PicturesWallProps {
-  setLocale: { upload: string };
   pictureFormateLimit: string;
 }
 
-export interface InternalPicturesWallState {
-  previewVisible: boolean;
-  activeIndex: number;
-  fileList: UploadFile[];
-}
+const PicturesWall: React.FC<PicturesWallProps> = (props) => {
+  const { getUrl: defaultGetUrl, pictureFormateLimit: defaultPictureFormateLimit } = useContext(ConfigContext);
+  const intl = useIntl();
 
-class InternalPicturesWall extends React.Component<InternalPicturesWallProps, InternalPicturesWallState> {
-  /** ref: https://reactjs.org/docs/react-component.html#static-getderivedstatefromprops */
-  static getDerivedStateFromProps(props: InternalPicturesWallProps) {
-    return {
-      fileList: setFileList(props),
-    };
-  }
+  const {
+    value,
+    getUrl = defaultGetUrl,
+    pictureFormateLimit = defaultPictureFormateLimit,
+    viewerProps,
+  } = props;
 
-  state = {
-    previewVisible: false,
-    activeIndex: 0,
-    fileList: [] as UploadFile[],
+  const files = setFileList({ value, getUrl });
+
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handlePreview = async (file: UploadFile) => {
+    console.log(files, file);
+    console.log(_findIndex(files, { uid: file.uid }));
+    setActiveIndex(_findIndex(files, { uid: file.uid }));
+    setPreviewVisible(true);
   };
 
-  handlePreview = async (file: UploadFile) => {
-    const { fileList } = this.state;
-    console.log(fileList, file);
-    console.log(_findIndex(fileList, { uid: file.uid }));
-    this.setState({
-      activeIndex: _findIndex(fileList, { uid: file.uid }),
-      previewVisible: true,
-    });
-  };
-
-  handleChange = ({ fileList }: { fileList: UploadFile[] }) => {
-    console.log(fileList);
-    const { onChange } = this.props;
+  const handleChange = ({ fileList }: { fileList: UploadFile[] }) => {
+    console.log('handleChange', fileList);
+    const { onChange } = props;
     if (onChange) {
       onChange(filterFileList(fileList) as any);
     }
   };
 
-  render() {
-    const { pictureFormateLimit, setLocale, viewerProps } = this.props;
-    const { previewVisible, fileList, activeIndex } = this.state;
-    const uploadButton = (
-      <div>
-        <PlusOutlined />
-        <div className="ant-upload-text">{_get(setLocale, 'upload')}</div>
-      </div>
-    );
-    return (
-      <div className={`${styles.pictureWall} clearfix`}>
-        <CustomUpload
-          accept={pictureFormateLimit}
-          {...this.props}
-          fileList={fileList}
-          onPreview={this.handlePreview}
-          onChange={this.handleChange}
-          listType="picture-card"
-        >
-          {fileList.length >= (this.props.filesCountLimit || 1) ? null : uploadButton}
-        </CustomUpload>
-        <ImagesViewer
-          visible={previewVisible}
-          images={fileList.map(item => ({ src: item.url!, alt: item.fileName }))}
-          onClose={() => this.setState({ previewVisible: false })}
-          activeIndex={activeIndex}
-          {...viewerProps}
-        />
-      </div>
-    );
-  }
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div className="ant-upload-text">{intl.getMessage('picturesWall.upload', '上传')}</div>
+    </div>
+  );
+
+  return (
+    <div className={`${styles.pictureWall} clearfix`}>
+      <CustomUpload
+        accept={pictureFormateLimit}
+        {...props}
+        fileList={files}
+        onPreview={handlePreview}
+        onChange={handleChange}
+        listType="picture-card"
+      >
+        {files.length >= (props.filesCountLimit || 1) ? null : uploadButton}
+      </CustomUpload>
+      <ImagesViewer
+        visible={previewVisible}
+        images={files.map(item => ({ src: item.url!, alt: item.fileName }))}
+        onClose={() => setPreviewVisible(false)}
+        activeIndex={activeIndex}
+        {...viewerProps}
+      />
+    </div>
+  );
 }
 
-export default forwardRef<React.ComponentClass, PicturesWallProps>((props, ref) => {
-  const { getUrl, pictureFormateLimit, afmLocale: { picturesWall } } = useContext(ConfigContext);
-  const forwardProps = {
-    getUrl,
-    pictureFormateLimit,
-    setLocale: {
-      upload: picturesWall.upload,
-    },
-    ...props,
-    ref,
-  } as any;
-  console.log(forwardProps);
-  return <InternalPicturesWall {...forwardProps} />;
-})
+export default PicturesWall;
