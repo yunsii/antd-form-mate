85ee47e2906db148760f761310eeee683585e350
theprimone <yuns.xie@qq.com>
2020-03-05 17:26:43 +0800

    fix: sync internal value with outer value, and use isNumber function

--- src/lib/item-components/InputNumberRange/index.tsx
+++ src/lib/item-components/InputNumberRange/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Input, InputNumber } from 'antd';
 import { InputNumberProps } from 'antd/lib/input-number';
-import { useUpdateEffect } from '../../hooks'
+import _get from 'lodash/get';
+import _isNumber from 'lodash/isNumber';
 
+import { useUpdateEffect } from '../../hooks'
 import { useIntl } from '../../../intl-context';
 
 export type CustomInputNumberProps = Omit<InputNumberProps, "style" | "value" | "onChange">
@@ -22,13 +24,25 @@ export interface InputNumberRangeProps {
 
 export const InputNumberRange: React.FC<InputNumberRangeProps> = (props) => {
   const intl = useIntl();
-  const { onChange, separator, number1Props, number2Props } = props;
+  const { value, onChange, separator, number1Props, number2Props } = props;
 
-  const [value1, setValue1] = useState<number>();
-  const [value2, setValue2] = useState<number>();
+  const getValue1 = () => _get(value, "[0]");
+  const getValue2 = () => _get(value, "[1]");
 
+  const [value1, setValue1] = useState<number | undefined>(getValue1());
+  const [value2, setValue2] = useState<number | undefined>(getValue2());
+
+  // 与外部数据同步，当 value 为受控状态时，同步到内部状态中
   useUpdateEffect(() => {
-    if (value1 && value2) {
+    if (_isNumber(getValue1()) && _isNumber(getValue2())) {
+      setValue1(getValue1());
+      setValue2(getValue2());
+    }
+  }, [value]);
+
+  // 内部数据同步，当两个值有变化时，如果两个值都有效，触发 `onChange` ，返回数组；否则返回 undefined
+  useUpdateEffect(() => {
+    if (_isNumber(value1) && _isNumber(value2)) {
       onChange?.([value1, value2]);
     } else {
       onChange?.();
@@ -45,7 +59,7 @@ export const InputNumberRange: React.FC<InputNumberRangeProps> = (props) => {
           textAlign: 'center',
         }}
         value={value1}
-        onChange={value => { setValue1(value) }}
+        onChange={_value => { setValue1(_value) }}
       />
       <Input
         style={{
@@ -66,7 +80,7 @@ export const InputNumberRange: React.FC<InputNumberRangeProps> = (props) => {
           borderLeft: 0,
         }}
         value={value2}
-        onChange={value => { setValue2(value) }}
+        onChange={_value => { setValue2(_value) }}
       />
     </Input.Group>
   );
