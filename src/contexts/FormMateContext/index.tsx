6ece67ceba473945bce21e82f7908a07b69e1f5f
zpr1g <yuns.xie@qq.com>
2020-04-07 10:42:36 +0800

    feat: FormMateContext

---
+++ src/contexts/FormMateContext/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export interface FormMateConsumerProps {
+  renderItem?: (item: React.ReactNode, name: string | null) => React.ReactNode;
+}
+
+export const FormMateContext = React.createContext<FormMateConsumerProps>({});
+
+FormMateContext.displayName = 'antd-form-mate\'s FormMateContext';
+
+/** 共享 `FormMate` 组件属性 */
+export default FormMateContext;
