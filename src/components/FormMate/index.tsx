9a599bcafb60b1db6d328fab25c50c93eb091f7f
zpr1g <yuns.xie@qq.com>
2020-04-07 17:03:10 +0800

    chore: remove create function

--- src/components/FormMate/index.tsx
+++ src/components/FormMate/index.tsx
@@ -5,15 +5,16 @@ import FormMateDynamic from "./FormMateDynamic";
 import { FormMate as InternalForm } from "./FormMate";
 
 type InternalForm = typeof InternalForm;
+
+/**
+ * 让表单使用更轻松
+ */
 interface FormMate extends InternalForm {
   useForm: typeof Form.useForm;
   Item: typeof FormMateItem;
   Dynamic: typeof FormMateDynamic;
   List: typeof Form.List;
   Provider: typeof Form.Provider;
-
-  /** @deprecated Only for warning usage. Do not use. */
-  create: () => void;
 }
 
 const FormMate: FormMate = InternalForm as FormMate;
@@ -23,6 +24,5 @@ FormMate.Item = FormMateItem;
 FormMate.Dynamic = FormMateDynamic;
 FormMate.List = Form.List;
 FormMate.Provider = Form.Provider;
-FormMate.create = Form.create;
 
 export default FormMate;
