696916bb90c00067d618e3f86511b94653cbfb94
zpr1g <yuns.xie@qq.com>
2020-04-08 11:38:02 +0800

    feat: generics with FormMateItem

--- src/components/FormMate/FormMateItem/index.tsx
+++ src/components/FormMate/FormMateItem/index.tsx
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
 import _get from 'lodash/get';
 import _isFunction from 'lodash/isFunction';
 import _find from 'lodash/find';
-import { Form } from "antd";
-import { FormInstance } from "antd/lib/form";
-import { FormItemProps } from "antd/lib/form/FormItem";
+import { Form } from 'antd';
+import { FormInstance } from 'antd/lib/form';
+import { FormItemProps } from 'antd/lib/form/FormItem';
 
-import { FormMateItemProps } from "../../../interfaces";
+import { FormMateItemProps, ComponentType, ComponentProps } from '../../../interfaces';
 import { ConfigContext } from '../../../contexts/ConfigContext/context';
 import { useIntl } from '../../../contexts/Intlcontext';
 // import { renderCol } from '../../FormMate/utils';
@@ -14,33 +14,27 @@ import getComponent from '../../../map';
 import { FormMateItemDisplayName } from '../../../constants/components';
 import { setValuePropName } from './utils';
 
-const FormMateItem: React.FC<FormMateItemProps> = ({
-  type = "string",
+const FormMateItem = <T extends ComponentType[keyof ComponentType], P extends ComponentProps>({
+  type = 'string' as any,
   name,
   componentProps,
   children,
   ...restFormItemProps
-}) => {
+}: FormMateItemProps<T, P>) => {
   const intl = useIntl();
   const { setCommonProps, commonExtra, commonRules } = useContext(ConfigContext);
 
-  const {
-    style,
-    dense,
-    extra,
-    wrapperCol,
-    labelCol,
-    rules,
-    ...rest
-  } = restFormItemProps;
+  const { style, dense, extra, wrapperCol, labelCol, rules, ...rest } = restFormItemProps;
 
   function setStyle() {
     return dense ? { marginBottom: 0, ...style } : style;
   }
 
   function setExtra() {
-    if (extra === false || extra === null) { return undefined; }
-    return extra || commonExtra[type];
+    if (extra === false || extra === null) {
+      return undefined;
+    }
+    return extra || commonExtra[type as ComponentType];
   }
 
   if (type === 'plain') {
@@ -51,37 +45,29 @@ const FormMateItem: React.FC<FormMateItemProps> = ({
       >
         {({ getFieldValue }: FormInstance) => {
           return (
-            <Form.Item
-              name={name}
-              style={setStyle()}
-              extra={setExtra()}
-              {...rest}
-            >
+            <Form.Item name={name} style={setStyle()} extra={setExtra()} {...rest}>
               <div className="ant-form-text">{getFieldValue(name!)}</div>
             </Form.Item>
-          )
+          );
         }}
       </Form.Item>
-    )
-  }
-
-  const typedComponent = getComponent(type);
-
-  function createElement(): FormItemProps['children'] {
-    if (type === 'custom') { return children; }
-    return (
-      React.cloneElement(typedComponent, {
-        ...setCommonProps(type, _get(typedComponent.props, 'style')),
-        ...componentProps,
-      })
     );
   }
 
+  const typedComponent = getComponent(type) as JSX.Element;
+
+  function createElement(): FormItemProps['children'] {
+    if (type === 'custom') {
+      return children;
+    }
+    return React.cloneElement(typedComponent, {
+      ...setCommonProps(type as ComponentType, _get(typedComponent.props, 'style')),
+      ...componentProps,
+    });
+  }
+
   function setRules() {
-    let result = [
-      ...commonRules[type] || [],
-      ...rules || [],
-    ]
+    let result = [...(commonRules[type as any] || []), ...(rules || [])];
 
     if (rest.required && !_find(result, { required: true })) {
       result = [
@@ -90,7 +76,7 @@ const FormMateItem: React.FC<FormMateItemProps> = ({
           message: `${rest.label} ${intl.getMessage('message.isRequired', '必填')}`,
         },
         ...result,
-      ]
+      ];
     }
 
     return result;
@@ -102,13 +88,13 @@ const FormMateItem: React.FC<FormMateItemProps> = ({
       style={setStyle()}
       extra={setExtra()}
       {...rest}
-      valuePropName={setValuePropName(type)}
+      valuePropName={setValuePropName(type as ComponentType)}
       rules={setRules()}
     >
       {createElement()}
     </Form.Item>
   );
-}
+};
 
 FormMateItem.displayName = FormMateItemDisplayName;
 
