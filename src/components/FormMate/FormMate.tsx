8a91a939da229f24a429552ff1d03396ad5bef41
zpr1g <yuns.xie@qq.com>
2020-04-21 23:34:21 +0800

    refactor: condition judge

--- src/components/FormMate/FormMate.tsx
+++ src/components/FormMate/FormMate.tsx
@@ -6,10 +6,20 @@ import _keys from 'lodash/keys';
 import { Form } from 'antd';
 
 import FormMateContext from '../../contexts/FormMateContext';
-import { FormMateDynamicDisplayName } from '../../constants/components';
+import { FormMateItemDisplayName, FormMateDynamicDisplayName } from '../../constants/components';
 import { getChildName } from './utils';
 import setInitialValue from '../../utils/setValue';
 import { FormMateProps } from '../../interfaces';
+
+function isFormItem(child: React.ReactNode): child is JSX.Element {
+  return (
+    React.isValidElement(child) && [FormMateItemDisplayName, FormMateDynamicDisplayName].includes(getChildName(child))
+  );
+}
+
+const isFormDynamic = (child: React.ReactNode) => {
+  return React.isValidElement(child) && FormMateDynamicDisplayName === getChildName(child);
+};
 
 export const FormMate = (props: FormMateProps) => {
   const { initialValues, renderChildren, renderItem, children, postInitialValues, ...rest } = props;
@@ -17,14 +27,16 @@ export const FormMate = (props: FormMateProps) => {
   const fieldsType = {};
 
   const renderItems = React.Children.map(children, (child) => {
-    if (React.isValidElement(child) && child.props.name) {
+    if (isFormItem(child)) {
       fieldsType[child.props.name] = child.props.type;
+
+      // 如果为动态类型字段，默认隐藏，显示的时候再调用 `renderItem` ，通过 `FormMateContext` 实现
+      if (isFormDynamic(child)) {
+        return child;
+      }
     }
 
-    // 如果为动态类型字段，默认隐藏，显示的时候再调用 `renderItem` ，通过 `FormMateContext` 实现
-    if (getChildName(child) === FormMateDynamicDisplayName) {
-      return child;
-    }
+    // TODO: loop is FormMateWrapper to set fieldsType;
     return renderItem ? renderItem(child, getChildName(child)) : child;
   });
 
