aa7fb839dea083eea3506a6f1fa47408910faa7f
zpr1g <yuns.xie@qq.com>
2020-04-26 15:42:25 +0800

    fix: fieldsTypeRef.current maybe undefined when setInitialValues

--- src/components/FormMate/utils.tsx
+++ src/components/FormMate/utils.tsx
@@ -109,7 +109,11 @@ export function useFormMate(formMate?: FormMateInstance): [FormMateInstance] {
           return null;
         },
         setInitialValues: (values: Store) => {
-          if (!storeRef.current) {
+          /**
+           * 使用 Popover 等组件时由于还未渲染 FormMate 组件， fieldsTypeRef.current 可能为 `undefined` ，
+           * 故与此类组件结合使用时，应在 FormMate 组件上直接配置 `initialValues` 或在组件显示时调用 `setInitialValues` 方法
+           */
+          if (!storeRef.current && fieldsTypeRef.current) {
             storeRef.current = processInitialValues(values, fieldsTypeRef.current, postProcessRef.current);
             antdForm.setFieldsValue(storeRef.current);
           }
