6227a45e51226eeefd7ea9c1a5e90dba96889d94
zpr1g <yuns.xie@qq.com>
2020-05-07 14:54:56 +0800

    feat!: simplify CustomSelect

--- src/components/CustomSelect/index.tsx
+++ src/components/CustomSelect/index.tsx
@@ -1,38 +1,28 @@
 import React, { forwardRef } from 'react';
-import { Select, Spin } from 'antd';
+import { Select } from 'antd';
 import { SelectProps } from 'antd/lib/select';
 import { OptionsType } from 'rc-select/lib/interface';
 
 export interface CustomSelectProps extends SelectProps<any> {
   options?: OptionsType;
-  disabledStyle?: React.CSSProperties;
-  loading?: boolean;
-  style?: React.CSSProperties;
-  placeholder?: string;
 }
 
 export default forwardRef<Select, CustomSelectProps>((props, ref) => {
-  const { options, loading, disabledStyle = { backgroundColor: 'rgba(0, 0, 0, 0.25)' }, ...rest } = props;
+  const { options, ...rest } = props;
 
   const renderOptions = (items: OptionsType) => {
     return items.map((item) => {
-      const { options, ...rest } = item;
+      const { options, ...itemRest } = item;
       if (options) {
         return (
-          <Select.OptGroup label={item.label} key={`${item.label}`} {...rest}>
+          <Select.OptGroup key={`${item.label}`} {...itemRest}>
             {renderOptions(options)}
           </Select.OptGroup>
         );
       }
 
       return (
-        <Select.Option
-          key={item.value}
-          value={item.value}
-          disabled={item.disabled}
-          {...rest}
-          style={item.disabled ? disabledStyle : {}}
-        >
+        <Select.Option key={item.value} value={item.value} {...itemRest}>
           {item.label}
         </Select.Option>
       );
@@ -40,10 +30,8 @@ export default forwardRef<Select, CustomSelectProps>((props, ref) => {
   };
 
   return (
-    <Spin spinning={loading || false}>
-      <Select {...rest} ref={ref}>
-        {options && renderOptions(options)}
-      </Select>
-    </Spin>
+    <Select {...rest} ref={ref}>
+      {options && renderOptions(options)}
+    </Select>
   );
 });
