3b22f8a497764b995671b9a887cefb8b338870b0
zpr1g <yuns.xie@qq.com>
2020-05-11 21:02:57 +0800

    fix: not process initialValues

--- src/components/FormMate/FormMate.tsx
+++ src/components/FormMate/FormMate.tsx
@@ -1,72 +1,99 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { Form, Row, Col } from 'antd';
 import { FormInstance } from 'antd/lib/form';
 
 import { INTERNAL_HOOK_MARK } from '../../constants/components';
 import FormMateContext from '../../contexts/FormMateContext';
-import { isFormItem, isFormDynamic, getFormItemName, useFormMate } from './utils';
-import { FormMateProps, FormMateItemProps, FormMateInstance, InternalFormMateInstance } from '../../interfaces';
+import {
+  isFormItem,
+  isFormDynamic,
+  getFormItemName,
+  useFormMate,
+} from './utils';
+import {
+  FormMateProps,
+  FormMateItemProps,
+  FormMateInstance,
+  InternalFormMateInstance,
+} from '../../interfaces';
 
-export const FormMate = React.forwardRef<FormMateInstance, FormMateProps>((props, ref) => {
-  const { initialValues, renderChildren, renderItem, children, postInitialValues, grid, formMate, ...rest } = props;
-  const [internalForm] = useFormMate(formMate);
-  const formRef = useRef<FormInstance>(null);
+export const FormMate = React.forwardRef<FormMateInstance, FormMateProps>(
+  (props, ref) => {
+    const {
+      renderChildren,
+      renderItem,
+      children,
+      postInitialValues,
+      grid,
+      formMate,
+      ...rest
+    } = props;
+    const [internalForm] = useFormMate(formMate);
+    const formRef = useRef<FormInstance>(null);
 
-  const _renderChildren = grid ? (_children: React.ReactNode) => <Row {...grid.row}>{_children}</Row> : renderChildren;
-  const _renderItem = (() => {
-    if (grid) {
-      return (child: React.ReactNode, name: FormMateItemProps['name'], index: number) => {
-        if (typeof grid.col === 'function') {
-          /** 如果 grid.col 函数无返回值，直接渲染 child ，可避免渲染无效节点与 Col 组件结合占位 */
-          if (grid.col(child, name, index)) {
-            return <Col {...grid.col(child, name, index)}>{child}</Col>;
+    const _renderChildren = grid
+      ? (_children: React.ReactNode) => <Row {...grid.row}>{_children}</Row>
+      : renderChildren;
+    const _renderItem = (() => {
+      if (grid) {
+        return (
+          child: React.ReactNode,
+          name: FormMateItemProps['name'],
+          index: number
+        ) => {
+          if (typeof grid.col === 'function') {
+            /** 如果 grid.col 函数无返回值，直接渲染 child ，可避免渲染无效节点与 Col 组件结合占位 */
+            if (grid.col(child, name, index)) {
+              return <Col {...grid.col(child, name, index)}>{child}</Col>;
+            }
+            return child;
           }
-          return child;
-        }
-        return <Col {...grid.col}>{child}</Col>;
-      };
+          return <Col {...grid.col}>{child}</Col>;
+        };
+      }
+      return renderItem;
+    })();
+
+    const fieldsType = {};
+    const formItems = React.Children.map(children, (child, index) => {
+      if (isFormItem(child) && child.props.name) {
+        fieldsType[child.props.name] = child.props.type;
+      }
+
+      // 如果为动态类型字段，默认隐藏，显示的时候再调用 `renderItem` ，通过 `FormMateContext` 实现
+      if (isFormDynamic(child)) {
+        return child;
+      }
+
+      return _renderItem
+        ? _renderItem(child, getFormItemName(child), index)
+        : child;
+    });
+
+    const {
+      setFieldsType,
+      setPostProcess,
+    } = (internalForm as InternalFormMateInstance).getFormMateInternalHook(
+      INTERNAL_HOOK_MARK
+    )!;
+
+    setFieldsType(fieldsType);
+    if (postInitialValues) {
+      setPostProcess(postInitialValues);
     }
-    return renderItem;
-  })();
 
-  const fieldsType = {};
-  const formItems = React.Children.map(children, (child, index) => {
-    if (isFormItem(child) && child.props.name) {
-      fieldsType[child.props.name] = child.props.type;
-    }
+    React.useImperativeHandle(ref, () => internalForm);
 
-    // 如果为动态类型字段，默认隐藏，显示的时候再调用 `renderItem` ，通过 `FormMateContext` 实现
-    if (isFormDynamic(child)) {
-      return child;
-    }
-
-    return _renderItem ? _renderItem(child, getFormItemName(child), index) : child;
-  });
-
-  const { setFieldsType, setPostProcess } = (internalForm as InternalFormMateInstance).getFormMateInternalHook(
-    INTERNAL_HOOK_MARK
-  )!;
-
-  setFieldsType(fieldsType);
-  if (postInitialValues) {
-    setPostProcess(postInitialValues);
+    return (
+      <FormMateContext.Provider
+        value={{
+          renderItem: _renderItem,
+        }}
+      >
+        <Form ref={formRef} form={internalForm} {...rest}>
+          {_renderChildren ? _renderChildren(formItems) : formItems}
+        </Form>
+      </FormMateContext.Provider>
+    );
   }
-
-  useEffect(() => {
-    initialValues && internalForm.setInitialValues(initialValues);
-  }, [initialValues]);
-
-  React.useImperativeHandle(ref, () => internalForm);
-
-  return (
-    <FormMateContext.Provider
-      value={{
-        renderItem: _renderItem,
-      }}
-    >
-      <Form ref={formRef} form={internalForm} {...rest}>
-        {_renderChildren ? _renderChildren(formItems) : formItems}
-      </Form>
-    </FormMateContext.Provider>
-  );
-});
+);
