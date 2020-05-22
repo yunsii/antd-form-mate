c3b0541f7cba7f645bbf7304f8865010dc701e84
zpr1g <yuns.xie@qq.com>
2020-05-22 15:30:59 +0800

    feat: onReload and addonAfter of Select

--- src/components/CustomSelect/index.tsx
+++ src/components/CustomSelect/index.tsx
@@ -1,14 +1,21 @@
 import React, { forwardRef } from 'react';
 import { Select } from 'antd';
+import { ReloadOutlined } from '@ant-design/icons';
 import { SelectProps } from 'antd/lib/select';
 import { OptionsType } from 'rc-select/lib/interface';
+import { cloneElement } from '../../utils/reactNode';
+
+import AddonWrapper from '../AddonWrapper';
+import styles from './index.less';
 
 export interface CustomSelectProps extends SelectProps<any> {
   options?: OptionsType;
+  addonAfter?: React.ReactNode;
+  onReload?: () => void;
 }
 
 export default forwardRef<Select, CustomSelectProps>((props, ref) => {
-  const { options, ...rest } = props;
+  const { options, addonAfter, onReload, ...rest } = props;
 
   const renderOptions = (items: OptionsType) => {
     return items.map((item) => {
@@ -29,9 +36,25 @@ export default forwardRef<Select, CustomSelectProps>((props, ref) => {
     });
   };
 
-  return (
+  const renderSelect = () => (
     <Select {...rest} ref={ref}>
       {options && renderOptions(options)}
     </Select>
+  );
+
+  if (!onReload) {
+    return renderSelect();
+  }
+
+  return (
+    <AddonWrapper
+      addonAfter={
+        addonAfter || (
+          <ReloadOutlined onClick={onReload} />
+        )
+      }
+    >
+      {cloneElement(renderSelect(), { className: styles['addon-select'] })}
+    </AddonWrapper>
   );
 });
