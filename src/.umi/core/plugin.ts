bc6e507f2cce1c1502b7d51c5850b354d02ac436
zpr1g <yuns.xie@qq.com>
2020-06-19 22:54:00 +0800

    chore: remove .umi

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/home/zpr1g/Workspaces/github/antd-form-mate/node_modules/_@umijs_runtime@3.2.3@@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
