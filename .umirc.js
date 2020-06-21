6e66632ecf18c6440a8420e6076db5c2877197a5
zpr1g <yuns.xie@qq.com>
2020-06-21 23:10:54 +0800

    feat: add navs

--- .umirc.js
+++ .umirc.js
@@ -14,4 +14,9 @@ export default {
   base: '/antd-form-mate',
   publicPath: '/antd-form-mate/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
+  navs: [
+    null,
+    { title: 'GitHub', path: 'https://github.com/zpr1g/antd-form-mate' },
+    { title: '更新日志', path: 'https://github.com/zpr1g/antd-form-mate/releases' },
+  ],
 };
