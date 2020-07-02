0336c5490e8f8d4c4aadb6e29eb6ac24d24235c7
zpr1g <yuns.xie@qq.com>
2020-07-02 16:41:13 +0800

    refactor: export

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,15 @@ export type { IntlProvider, IntlConsumer, createIntl, IntlType, zhCNIntl, enUSIn
 import FormMate from './components/FormMate';
 export default FormMate;
 
-export { registerComponent } from './map';
-
-export { default as Upload } from './components/commons/CustomUpload';
+export { default as CoreUpload } from './components/CoreUpload';
 export { default as ImagesViewer } from './components/commons/ImagesViewer';
 
 export { default as CheckGroup } from './components/CustomCheckGroup';
 export { default as DatePicker } from './components/CustomDatePicker';
 export { default as RadioGroup } from './components/CustomRadioGroup';
 export { default as Select } from './components/CustomSelect';
-export { default as Dragger } from './components/CustomDragger';
-export { default as PicturesWall } from './components/PicturesWall';
+export { default as UploadDragger } from './components/CustomUploadDragger';
+export { default as CustomUpload } from './components/CustomUpload';
 export { default as InputNumberRange } from './components/InputNumberRange';
 
 export { getBase64, getImageDimension, progressXhr } from './utils/commons';
