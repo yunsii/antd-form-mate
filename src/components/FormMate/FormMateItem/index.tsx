ae9fb3b7a9a218580e1d225ff891d284b7e0b1e2
zpr1g <yuns.xie@qq.com>
2020-07-02 16:38:36 +0800

    refactor: FormMateItem

--- src/components/FormMate/FormMateItem/index.tsx
+++ src/components/FormMate/FormMateItem/index.tsx
@@ -1,35 +1,35 @@
 import React, { useContext } from 'react';
 import _get from 'lodash/get';
 import _find from 'lodash/find';
+import _omit from 'lodash/omit';
 import { Form } from 'antd';
-import { FormInstance } from 'antd/lib/form';
-import { FormItemProps } from 'antd/lib/form/FormItem';
+import { FormInstance, FormItemProps } from 'antd/lib/form';
 
-import { FormMateItemProps, ComponentType } from '../../../interfaces';
-import { ConfigContext } from '../../../contexts/ConfigContext/context';
 import FormMateContext from '../../../contexts/FormMateContext';
 import { useIntl } from '../../../contexts/Intlcontext';
-import getComponent from '../../../map';
 import { FormMateItemDisplayName } from '../../../constants/components';
-import { setValuePropName } from './utils';
-import defaultPlainRender from '../../../utils/plainRender';
 
-const FormMateItem = <T, P = {}>({
-  type = 'string',
-  name,
-  componentProps,
-  children,
-  ...restFormItemProps
-}: FormMateItemProps<T, P>) => {
+export type PlainRenderFn<P = Object> = (item: {
+  value: any;
+  name: NewFormMateItemProps<P>['name'];
+  entryProps: P;
+}) => React.ReactNode;
+
+export interface NewFormMateItemProps<P = Object> extends FormItemProps {
+  entryProps?: P;
+  plain?: boolean;
+  plainRender?: PlainRenderFn<P>;
+  dynamicRender?: (form: FormInstance) => boolean | void;
+  dense?: boolean;
+}
+
+export type NewFormMateItemPropsWithoutChildren<P = Object> = Omit<NewFormMateItemProps<P>, 'children'>;
+
+const FormMateItem = <P,>(props: NewFormMateItemProps<P>) => {
   const intl = useIntl();
-  const { setCommonProps, commonExtra, commonRules } = useContext(ConfigContext);
-  const { type: contextType, plainRender } = useContext(FormMateContext);
+  const { renderItem } = useContext(FormMateContext);
 
-  const { style, dense, extra, rules, ...rest } = restFormItemProps;
-
-  function getType() {
-    return (contextType || type) as ComponentType;
-  }
+  const { name, children, style, dense, extra, rules, plain, plainRender, dynamicRender, entryProps, ...rest } = props;
 
   function getStyle() {
     return dense ? { marginBottom: 0, ...style } : style;
@@ -37,21 +37,27 @@ const FormMateItem = <T, P = {}>({
 
   if (!name) {
     /** 如果没有 `name` ，尚可使用 `dense` 属性 */
+    return <Form.Item {...props} style={getStyle()} />;
+  }
+
+  if (dynamicRender) {
     return (
-      <Form.Item style={getStyle()} {...restFormItemProps}>
-        {children}
+      <Form.Item noStyle shouldUpdate={rest.shouldUpdate || true}>
+        {(form: FormInstance) => {
+          if (dynamicRender(form)) {
+            return renderItem ? (
+              renderItem(<FormMateItem {..._omit(props, 'dynamicRender')}>{children}</FormMateItem>, name)
+            ) : (
+              <FormMateItem {..._omit(props, 'dynamicRender')}>{children}</FormMateItem>
+            );
+          }
+          return null;
+        }}
       </Form.Item>
     );
   }
 
-  function setExtra() {
-    if (extra === false || extra === null) {
-      return undefined;
-    }
-    return extra || commonExtra[getType()];
-  }
-
-  if (getType() === 'plain') {
+  if (plain) {
     return (
       <Form.Item
         noStyle
@@ -60,38 +66,15 @@ const FormMateItem = <T, P = {}>({
         {({ getFieldValue }: FormInstance) => {
           const itemValue = getFieldValue(name!);
 
-          const _plainRender = () => {
-            /** 自定义组件时，注入 `__renderType__` 属性，考虑不周，谨慎使用 */
-            if (type === 'custom' && React.isValidElement(children)) {
-              return React.cloneElement(children, {
-                __renderType__: 'plain',
-              });
-            }
-
-            /** 优先使用自定义渲染 */
-            const result =
-              plainRender &&
-              plainRender({
-                type: type as ComponentType,
-                name,
-                value: itemValue,
-                componentProps: componentProps || {},
-              });
-
-            if (result) {
-              return result;
-            }
-
-            return defaultPlainRender({
-              type: type as ComponentType,
-              name,
-              value: itemValue,
-              componentProps: componentProps || {},
-            });
-          };
           return (
-            <Form.Item name={name} style={getStyle()} extra={setExtra()} {...rest}>
-              <div className='ant-form-text'>{_plainRender()}</div>
+            <Form.Item name={name} style={getStyle()} {...rest}>
+              <div className='ant-form-text'>
+                {plainRender?.({
+                  name,
+                  value: itemValue,
+                  entryProps: entryProps || ({} as P),
+                }) || 'No plainRender.'}
+              </div>
             </Form.Item>
           );
         }}
@@ -99,30 +82,8 @@ const FormMateItem = <T, P = {}>({
     );
   }
 
-  const useCommonStyle = () => {
-    if (getType() === 'switch') {
-      return false;
-    }
-    // if (children) {
-    //   return false;
-    // }
-    return true;
-  };
-
-  const typedComponent = getComponent(getType()) as JSX.Element;
-
-  function createElement(): FormItemProps['children'] {
-    if (getType() === 'custom') {
-      return children;
-    }
-    return React.cloneElement(typedComponent, {
-      ...setCommonProps(getType(), _get(typedComponent.props, 'style'), useCommonStyle()),
-      ...componentProps,
-    }) as FormItemProps['children'];
-  }
-
   function setRules() {
-    let result = [...(commonRules[getType()] || []), ...(rules || [])];
+    let result = [...(rules || [])];
 
     if (rest.required && !_find(result, { required: true })) {
       result = [
@@ -138,16 +99,8 @@ const FormMateItem = <T, P = {}>({
   }
 
   return (
-    <Form.Item
-      name={name}
-      style={getStyle()}
-      extra={setExtra()}
-      {...rest}
-      valuePropName={setValuePropName(getType())}
-      rules={setRules()}
-    >
-      {createElement()}
-      {/* {children} */}
+    <Form.Item name={name} {...rest} style={getStyle()} rules={setRules()}>
+      {children}
     </Form.Item>
   );
 };
