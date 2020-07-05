50c0ce7b618ce0b8debeeb6384617b5d7f26e736
zpr1g <yuns.xie@qq.com>
2020-07-05 22:56:49 +0800

    refactor: getTargetOption

--- src/entries/CascaderEntry.tsx
+++ src/entries/CascaderEntry.tsx
@@ -3,35 +3,31 @@ import { Cascader } from 'antd';
 import { CascaderProps, CascaderOptionType } from 'antd/lib/cascader';
 import _find from 'lodash/find';
 
-import { getEntryDisplayName } from './utils';
+import { getEntryDisplayName, getTargetOption } from './utils';
 import FormMateItem, { NewFormMateItemPropsWithoutChildren, PlainRenderFn } from '../components/FormMate/FormMateItem';
 
 export interface CascaderEntryProps extends NewFormMateItemPropsWithoutChildren<CascaderProps> {}
 
-const getOption = (value: any, options: any) => {
-  return _find(options, { value });
-};
-
 const plainRender: PlainRenderFn<CascaderProps> = ({ name, value, entryProps }) => {
   const { options: cascaderOptions, displayRender = (labels) => labels.join(' / ') } = entryProps;
 
-  const labels: string[] = [];
+  const labels: React.ReactNode[] = [];
   const selectedOptions: CascaderOptionType[] = [];
 
-  const loopOptions = (_options: CascaderOptionType, index = 0) => {
+  const loopOptions = (_options: CascaderOptionType[], index = 0) => {
     console.log(value?.[index], _options);
-    const target = getOption(value?.[index], _options)!;
+    const target = getTargetOption(value?.[index], _options)!;
 
     labels.push(target.label);
     selectedOptions.push(target);
 
     if (index + 1 < value.length) {
-      loopOptions(target.children, index + 1);
+      loopOptions(target.children!, index + 1);
     }
   };
   loopOptions(cascaderOptions);
 
-  return displayRender(labels, selectedOptions);
+  return displayRender(labels as any, selectedOptions);
 };
 
 const CascaderEntry: React.FC<CascaderEntryProps> = (props) => {
--- src/entries/utils.ts
+++ src/entries/utils.ts
@@ -1,5 +1,17 @@
+import _find from 'lodash/find';
+
 import { ENTRY_PREFIX } from '../constants/components';
 
 export function getEntryDisplayName(entry: Function) {
   return `${ENTRY_PREFIX}.${entry.name}`;
+}
+
+export interface Option {
+  value?: any;
+  label?: React.ReactNode;
+  children?: Option[];
+}
+
+export function getTargetOption(value: any, options: Option[]) {
+  return _find(options, { value });
 }
