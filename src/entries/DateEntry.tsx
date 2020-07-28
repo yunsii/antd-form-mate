609b72b3e83444fe34eab3d95b8c0c56bcbf26ef
zpr1g <yuns.xie@qq.com>
2020-07-28 22:31:44 +0800

    feat: plainRender

--- src/entries/DateEntry.tsx
+++ src/entries/DateEntry.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { DatePicker } from 'antd';
+import { DatePickerProps } from 'antd/lib/date-picker';
+import { Moment } from 'moment';
 
 import { getEntryDisplayName } from './utils';
-import CustomDatePicker, { CustomDatePickerProps } from '../components/CustomDatePicker';
-import FormMateItem, { NewFormMateItemPropsWithoutChildren } from '../components/FormMate/FormMateItem';
+import FormMateItem, { NewFormMateItemPropsWithoutChildren, PlainRenderFn } from '../components/FormMate/FormMateItem';
 
-export interface DateEntryProps extends NewFormMateItemPropsWithoutChildren<CustomDatePickerProps> {}
+export interface DateEntryProps extends NewFormMateItemPropsWithoutChildren<Moment, DatePickerProps> {}
+
+const plainRender: PlainRenderFn<Moment | undefined, DatePickerProps> = ({ value, entryProps }) => {
+  const { showTime, format } = entryProps as any;
+  return value?.format((format as string) || (showTime ? 'YYYY-MM-DD HH:mm:ss' : 'YYYY-MM-DD'));
+};
 
 const DateEntry: React.FC<DateEntryProps> = (props) => {
   return (
-    <FormMateItem {...props}>
-      <CustomDatePicker {...props.entryProps} />
+    <FormMateItem plainRender={plainRender} {...props}>
+      <DatePicker picker='date' {...props.entryProps} />
     </FormMateItem>
   );
 };
