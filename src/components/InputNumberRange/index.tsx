7726c0cc6749b8657119e893e40deafbb2a6913e
zpr1g <yuns.xie@qq.com>
2020-08-02 17:34:11 +0800

    refactor: NumberValue, like DateRangeValue

--- src/components/InputNumberRange/index.tsx
+++ src/components/InputNumberRange/index.tsx
@@ -10,14 +10,12 @@ export type CustomInputNumberProps = Omit<InputNumberProps, 'value' | 'onChange'
 const separatorWidth = 36;
 const inputWidth = `calc(50% - ${separatorWidth / 2}px)`;
 
-export interface NumberRangeValue {
-  min?: number;
-  max?: number;
-}
+export type NumberValue<T> = T | null;
+export type RangeValue = [NumberValue<number>, NumberValue<number>] | null;
 
 export interface InputNumberRangeProps {
-  value?: NumberRangeValue;
-  onChange?: (value?: NumberRangeValue) => void;
+  value?: RangeValue;
+  onChange?: (value?: RangeValue) => void;
   separator?: string;
   placeholder?: [string, string];
   number1Props?: CustomInputNumberProps;
@@ -30,28 +28,19 @@ export const InputNumberRange: React.FC<InputNumberRangeProps> = (props) => {
   const foucsRef = useRef<boolean>(false);
 
   const setValue1 = (v1: number | undefined) => {
-    onChange?.({
-      min: v1 || undefined,
-      max: value?.max,
-    });
+    onChange?.([v1 || null, value?.[1] || null]);
   };
 
   const setValue2 = (v2: number | undefined) => {
-    onChange?.({
-      min: value?.min,
-      max: v2 || undefined,
-    });
+    onChange?.([value?.[0] || null, v2 || null]);
   };
 
   const handleBlur = () => {
     foucsRef.current = false;
 
     const timer = setTimeout(() => {
-      if (!foucsRef.current && value?.min && value?.max && value.min > value.max) {
-        onChange?.({
-          min: value.max,
-          max: value.min,
-        });
+      if (!foucsRef.current && value?.[0] && value?.[1] && value[0] > value[1]) {
+        onChange?.([value[1], value[0]]);
       }
       clearTimeout(timer);
     });
@@ -72,9 +61,9 @@ export const InputNumberRange: React.FC<InputNumberRangeProps> = (props) => {
           width: inputWidth,
           ...number1Props?.style,
         }}
-        value={value?.min}
+        value={value?.[0] || undefined}
         onChange={(_value) => {
-          setValue1(_value);
+          setValue1(_value as any);
         }}
       />
       <InputNumber
@@ -92,9 +81,9 @@ export const InputNumberRange: React.FC<InputNumberRangeProps> = (props) => {
           width: inputWidth,
           ...number2Props?.style,
         }}
-        value={value?.max}
+        value={value?.[1] || undefined}
         onChange={(_value) => {
-          setValue2(_value);
+          setValue2(_value as any);
         }}
       />
     </Input.Group>
